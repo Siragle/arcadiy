9c62399090df41199984fefa7048b9a87cc2e173
kurasixo <kurasixo@gmail.com>
2016-12-13 14:40:17 +0300

    added vies

--- dev/arcadiy/users/views.py
+++ dev/arcadiy/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import	HttpResponse
+from users.models import User
 
-def index(request):
-	return HttpResponse("Hello db")
+class UserList(ListView):
+	model = User
+	context_object_name = 'users'
